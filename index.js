a8a42df5cfe2608c6135610856f14f7eba358608
Anne Chen <annechen.anne@gmail.com>
2019-01-17 12:51:30 -0700

    added comment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //   * Randomly selects a word and uses the `Word` constructor to store it
 
 //   * Prompts the user for each guess and keeps track of the user's remaining guesses
+//project is also in my portfolio.
 //installing inquirer
 const inquirer = require('inquirer');
 //calling the Word constructor
